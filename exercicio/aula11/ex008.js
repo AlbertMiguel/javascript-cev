ca235cbaefe915f5a6d0a2baa8e72bf702a14b64
Albert Mike <miguelalbert38@gmail.com>
2022-11-08 17:41:55 -0300

    SIM

---
+++ exercicio/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 47
+console.log(`A velocidade o seu carro é ${vel}Km/h`)
+if (vel > 60) {// Condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija usando o cinto de segurança`)
