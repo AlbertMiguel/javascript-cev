50e1200290cfa90a080d3b5e5e7b72bdedd6f1a5
Albertin38 <miguelalbert38@gmail.com>
2022-11-09 11:20:26 -0300

    sim

---
+++ exercicio/aula13/ambiente.js
@@ -0,0 +1,31 @@
+var c = 8000
+    do {
+console.log(`Passo ${c}`)
+c++
+} while(c<=9000)
+ 
+
+
+
+
+
+
+
+console.log('TU É BRABO PARCEIRO')
+console.log(`O valor da minha variavel fica com ${c}`)
+
+
+
+
+
+
+
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
